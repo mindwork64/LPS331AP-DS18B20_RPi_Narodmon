5c4d0cab365457dd329c3d930c67b3adb89d9c05
Vitaly Murashov <34349377+mindwork64@users.noreply.github.com>
2017-12-23 00:45:50 +0300

    Update narmon.py

--- narmon.py
+++ narmon.py
@@ -7,6 +7,7 @@ import os
 
 # MAC адрес сетевой карты устройства. Заменить на свой!
 DEVICE_MAC = '00:00:00:00:00:00'
+LP_ADDR = '0x5d'
 
 #Имена датчиков. Если надо больше или меньше соотвественно добавляем или удаляем строку, не забывая изменить номер.
 SENSOR_ID_1 = 'T1'
@@ -14,9 +15,12 @@ SENSOR_ID_2 = 'T2'
 SENSOR_ID_3 = 'P1'
 
 #Читаем значения датчиков
+
+#Первым идёт DS18B20, если другой, меняем код под себя.
 temperature = []
 IDs = []
 for filename in os.listdir("/sys/bus/w1/devices"):
+  #В следующей строке меняем адрес датчика на свой.
   if fnmatch.fnmatch(filename, '28-0317621d49ff'):
     with open("/sys/bus/w1/devices/" + filename + "/w1_slave") as fileobj:
         lines = fileobj.readlines()
@@ -28,16 +32,16 @@ for filename in os.listdir("/sys/bus/w1/devices"):
           logger.error("Error reading sensor with ID: %s" % (filename))
 
 bus = smbus.SMBus(1)
-bus.write_byte_data(0x5c, 0x20, 0b10000100)
-bus.write_byte_data(0x5c,0x21, 0b1)
-Temp_LSB = bus.read_byte_data(0x5c, 0x2b)
-Temp_MSB = bus.read_byte_data(0x5c, 0x2c)
+bus.write_byte_data(LP_ADDR, 0x20, 0b10000100)
+bus.write_byte_data(LP_ADDR,0x21, 0b1)
+Temp_LSB = bus.read_byte_data(LP_ADDR, 0x2b)
+Temp_MSB = bus.read_byte_data(LP_ADDR, 0x2c)
 count = (Temp_MSB << 8) | Temp_LSB
 comp = count - (1 << 16)
 Temp = 42.5 + (comp/480.0)
-Pressure_LSB = bus.read_byte_data(0x5c, 0x29)
-Pressure_MSB = bus.read_byte_data(0x5c, 0x2a)
-Pressure_XLB = bus.read_byte_data(0x5c, 0x28)
+Pressure_LSB = bus.read_byte_data(LP_ADDR, 0x29)
+Pressure_MSB = bus.read_byte_data(LP_ADDR, 0x2a)
+Pressure_XLB = bus.read_byte_data(LP_ADDR, 0x28)
 count = (Pressure_MSB << 16) | ( Pressure_LSB << 8 ) | Pressure_XLB
 Pressure = ((count/4096.0)/1000)*750.064
 
